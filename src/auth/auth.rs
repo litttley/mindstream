4c11ac8a9264d60d94f3aa0bf0199c0421115ac1
Egor Berezovskiy <egor.neon@gmail.com>
2018-05-10 14:38:47 +0200

    upgrade dependencies

--- src/auth/auth.rs
+++ src/auth/auth.rs
@@ -1,6 +1,5 @@
 use actix_web::{FromRequest, HttpRequest, HttpMessage};
 use actix_web::Error;
-use futures::future::{result, FutureResult};
 
 use auth::jwt::{Claime, decode_token};
 use app::config;
@@ -19,17 +18,15 @@ impl Auth {
 
 impl<S> FromRequest<S> for Auth where S: 'static {
     type Config = ();
-    type Result = FutureResult<Self, Error>;
+    type Result = Result<Self, Error>;
 
     fn from_request(req: &HttpRequest<S>, _: &Self::Config) -> Self::Result {
-        result(
-            req.headers()
-                .get("Authorization")
-                .ok_or_else(|| errors::Error::Unauthorized)
-                .and_then(|token| token.to_str().map_err(|_| errors::Error::Unauthorized))
-                .and_then(|token| decode_token(token, &config::CONFIG.secret_key))
-                .map(Auth::new)
-                .map_err(|e| e.into())
-        )
+        req.headers()
+            .get("Authorization")
+            .ok_or_else(|| errors::Error::Unauthorized)
+            .and_then(|token| token.to_str().map_err(|_| errors::Error::Unauthorized))
+            .and_then(|token| decode_token(token, &config::CONFIG.secret_key))
+            .map(Auth::new)
+            .map_err(|e| e.into())
     }
 }
