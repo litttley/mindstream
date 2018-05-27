4833b99de48c996f2c2aa60c788e9f3358ec4eef
Egor Berezovskiy <egor.neon@gmail.com>
2018-05-27 23:20:13 +0200

    refactor build

---
+++ build.sh
@@ -0,0 +1,12 @@
+#/bin/bash -e
+
+echo "Run build client"
+cd client && npm install && npm run build && cd ..
+
+echo "Run build mobile"
+cd mobile && npm install && npm run build && cd ..
+
+echo "Run test server"
+cargo test --all
+echo "Run build server"
+cargo build --all
