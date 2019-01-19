e12c0b4c2c4ce7ce0b2eb23eb9c87fd206b12c4a
Egor Berezovskiy <egor.neon@gmail.com>
2019-01-19 14:29:04 +0100

    Travis (#277)

--- build.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash -e
-
-echo "Run build client"
-cd client && npm install && npm run tslint && npm run build && cd ..
-
-echo "Run test server"
-cargo test --all
-echo "Run build server"
-cargo build --all
