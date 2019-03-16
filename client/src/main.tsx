4337cb07f47231cb507ae3538c0cbabbb2bb880e
egor <egor.neon@gmail.com>
2019-03-16 13:12:03 +0100

    fix no-import-side-effect tslint rule

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,9 @@
+// tslint:disable: no-import-side-effect
 import "core-js/es/promise"
 import "core-js/es/array/find"
 import "~/reset.css"
 import "~/guide-styles.css"
+// tslint:enable: no-import-side-effect
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { App } from "~/App"
