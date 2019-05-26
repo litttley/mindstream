40f03f264e5373cd3058fc14cbbe406c110fb9d8
egor <egor.neon@gmail.com>
2019-05-26 09:34:02 +0200

    wip

---
+++ client/src/rssFeeds/Menu.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemIcon from "@material-ui/core/ListItemIcon"
+import ListItemText from "@material-ui/core/ListItemText"
+import Divider from "@material-ui/core/Divider"
+import StarIcon from "@material-ui/icons/Star"
+import RssFeedIcon from "@material-ui/icons/RssFeed"
+
+import { useIntlMessage } from "~/hooks/useIntlMessage"
+
+export function Menu() {
+  const message = useIntlMessage()
+
+  return (
+    <>
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <StarIcon />
+          </ListItemIcon>
+          <ListItemText primary={message("menu.feeds")} />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <RssFeedIcon />
+          </ListItemIcon>
+          <ListItemText primary={message("menu.rssSources")} />
+        </ListItem>
+      </List>
+      <Divider />
+    </>
+  )
+}
